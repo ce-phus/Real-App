5090925c730f7025127724b1d217d6b9a0da1a13
cephus <pnakitaren2@gmail.com>
2024-05-18 06:09:50 -0400

    task:configured app, ratings and common apps

--- backend/real_estate/real_estate/urls.py
+++ backend/real_estate/real_estate/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("api/v1/ratings/", include("apps.ratings.urls")),
+    path("api/v1/profile/", include("apps.profiles.urls"))
 ]
+
+admin.site.site_header = "Cephus Real Estate Admin"
+admin.site.site_title = "Cephus Real Estate Admin Portal"
+admin.site.index_title = "Welcome to the Cephus Real Estate Portal"
