e7d31226e22f2104c17427768b1c8c19968b8b79
cephus <pnakitaren2@gmail.com>
2024-05-18 03:53:52 -0400

    chpre: Created users model

--- backend/real_estate/apps/users/managers.py
+++ backend/real_estate/apps/users/managers.py
@@ -11,30 +11,59 @@ class CustomUserManager(BaseUserManager):
         except ValidationError:
             raise ValueError(_("You must rovide a valid email address"))
         
-        def create_user(
-                self, username, first_name, last_name, email, password, **extra_fields
-        ):
-            if not username:
-                raise ValueError(_("Users must submit a username"))
-            
-            if not first_name:
-                raise ValueError(_("Users must submit a first name"))
-            
-            if not last_name:
-                raise ValueError(_("Users must submit a last name"))
-            
-            if email:
-                email = self.normalize_email(email)
-                self.email_validator(email)
-            else:
-                raise ValueError(_("Base User Account: An email address is required"))
-            
-            # user  = self.model(
-            #     username=username
-            #     first_name=first_name
-            #     last_name=last_name
-            #     email=email
-            #     **extra_fields
-            # )
-            # user.set_pass
+    def create_user(
+            self, username, first_name, last_name, email, password, **extra_fields
+    ):
+        if not username:
+            raise ValueError(_("Users must submit a username"))
+        
+        if not first_name:
+            raise ValueError(_("Users must submit a first name"))
+        
+        if not last_name:
+            raise ValueError(_("Users must submit a last name"))
+        
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_("Base User Account: An email address is required"))
+        
+        user  = self.model(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        user.save(using=self._db)
+
+    def create_superuser(
+            self, username, first_name, last_name, email, password, **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superusers must have staff credentials"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("SUoerusers must be superusers"))
+        
+        if not password:
+            raise ValueError(_("SUperusers must have a password"))
+        
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+
+        else:
+            raise ValueError(_("Admin Account: An email address is required"))
+        user = self.create_user(
+            username, first_name, last_name, email, password, **extra_fields
+        )
+        return user
 
--- backend/real_estate/real_estate/settings.py
+++ backend/real_estate/real_estate/settings.py
@@ -186,34 +186,39 @@ logger = logging.getLogger(__name__)
 
 LOG_LEVEL = "INFO"
 
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "console": {
-                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+try:
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+                },
+                "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+                "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
             },
-            "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
-            "django.server": DEFAULT_LOGGING["formatters"]["django.server"]
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "console",
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "console",
+                },
+                "file": {
+                    "level": "INFO",
+                    "class": "logging.FileHandler",
+                    "formatter": "file",
+                    "filename": "logs/real_estate.log",
+                },
+                "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
             },
-            "file": {
-                "level": "INFO",
-                "class": "logging.FileHandler",
-                "formatter": "file",
-                "filename": "logs/real_estate.log",
+            "loggers": {
+                "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
+                "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
+                "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
             },
-            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-        },
-        "loggers": {
-            "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
-            "apps": {"level": "INFO", "handlers": ["console"], "propagate":False},
-            "django.server": DEFAULT_LOGGING["loggers"]["django.server"]
         }
-    }
-)
+    )
+except Exception as e:
+    print(f"Failed to configure logging: {e}")
+
+
